cee414babc2fc943b9f6211f7efef35ca3a0988e
Carlos Corona <29994688+Carlos-Corona@users.noreply.github.com>
2023-09-24 17:50:28 -0600

    Add new repo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/',
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    mode: 'development',
+    watch: true,
+    watchOptions: {
+      poll: 1000,
+      ignored: ['node_modules']
+    },
+  }
 })
